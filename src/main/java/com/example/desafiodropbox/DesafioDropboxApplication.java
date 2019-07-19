85a6a7f69792d94581b57d5bdd2d5f0aaf12f54f
cristian <cristian.a.s.andrade@hotmail.com>
2019-07-19 17:32:05 -0300

    criando a classe de modelo

--- src/main/java/com/example/desafiodropbox/DesafioDropboxApplication.java
+++ src/main/java/com/example/desafiodropbox/DesafioDropboxApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DesafioDropboxApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run ( DesafioDropboxApplication.class, args );
+
+
     }
+
 
 }
