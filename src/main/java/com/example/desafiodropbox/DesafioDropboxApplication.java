f01f5817aee103cfad408416ca8c3aeddc9b38ec
cristian <cristian.a.s.andrade@hotmail.com>
2019-07-19 12:53:42 -0300

    first commit

---
+++ src/main/java/com/example/desafiodropbox/DesafioDropboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafiodropbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDropboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run ( DesafioDropboxApplication.class, args );
+    }
+
+}
---
+++ src/main/java/com/example/desafiodropbox/models/User.java
@@ -0,0 +1,11 @@
+package com.example.desafiodropbox.models;
+
+import java.util.Date;
+
+public class User {
+
+    private String name;
+    private String document;
+    private Date dateBirth;
+
+}
